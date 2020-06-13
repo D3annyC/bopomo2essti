ffcf9218818ab3ae86c5b9ab85e300e66f5b45fe
D3annyC <ch.chung127@gmail.com>
2020-06-13 23:53:53 +0900

    #8 - Build express js mod: home page work

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
 const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
+const logger = require('./middleware/logger');
+const bopomo = require('./public/bopomo');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const PORT = process.env.PORT || 3000;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// Loogger
+app.use(logger);
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Build Main page Router
+app.get('/', (req, res) => {
+    res.render('index', { bopomo });
+});
+
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// API Routes
+app.use('/api/bopomo', require('./routes/api/bopomo'));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+app.listen(PORT, () => console.log(`Server runs on port ${PORT}`));
 module.exports = app;
