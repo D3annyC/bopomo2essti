7e6a4cc402567497cc8026663600c8045eadbfae
D3annyC <ch.chung127@gmail.com>
2020-06-24 20:21:57 +0900

    #8 fix:make js function work again

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const expbs = require('express-handlebars');
 const path = require('path');
+const router = require('./routes/routing');
 
 const PORT = process.env.PORT || 3000;
 const hbs = expbs.create({
@@ -10,33 +11,11 @@ const hbs = expbs.create({
 });
 
 app.use(express.static('public'));
+app.use('/', router);
 
 // Set up templet engining
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-// Router
-const router = express.Router();
-app.use('/', router);
-
-router.get('/', (req, res) => {
-    res.render('index',
-        {
-            tittle: '大家來學ㄅㄆㄇ',
-            style: 'style.css'
-        });
-});
-
-router.get('/bopomo2voice', (req, res) => {
-    res.render('bopomo2voice',
-        {
-            tittle: '大家來學ㄅㄆㄇ',
-            style: 'style.css'
-        });
-});
-
-// // Loogger
-// app.use(logger);
 
 
 app.listen(PORT, () => console.log(`Server runs on port ${PORT}`));
